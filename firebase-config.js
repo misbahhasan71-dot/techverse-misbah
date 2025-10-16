af596aac66efa0965b30da2ec52088232d791a28
misbahhasan71-dot <misbahhasan71@gmail.com>
2025-10-16 13:15:34 +0530

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// ==============================
+// ✅ Firebase Configuration
+// ==============================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBN5AMN7j_qnlnwulFHvIeR1NmLyQ0OZo",
+  authDomain: "techverse-ca88a.firebaseapp.com",
+  databaseURL: "https://techverse-ca88a-default-rtdb.firebaseio.com",
+  projectId: "techverse-ca88a",
+  storageBucket: "techverse-ca88a.firebasestorage.app",
+  messagingSenderId: "288969741715",
+  appId: "1:288969741715:web:03ddb8b82da9224e234466",
+  measurementId: "G-33XTL4HJR9"
+};
+
+// ✅ Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// ✅ Create reusable Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+console.log("🔥 Firebase initialized successfully");
