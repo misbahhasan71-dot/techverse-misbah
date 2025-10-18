f380fbf04b3af223663e67917fb7674b4c8abf18
misbahhasan71-dot <misbahhasan71@gmail.com>
2025-10-18 09:27:13 +0530

    Remove Firebase configuration from firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,1 @@
-// firebase-config.js 
-const firebaseConfig = {
-  apiKey: "AIzaSyCBN5AMN7j_qnlnwulFHvIeR1NmLyQ0OZo",
-  authDomain: "techverse-ca88a.firebaseapp.com",
-  databaseURL: "https://techverse-ca88a-default-rtdb.firebaseio.com",
-  projectId: "techverse-ca88a",
-  storageBucket: "techverse-ca88a.firebasestorage.app",
-  messagingSenderId: "288969741715",
-  appId: "1:288969741715:web:03ddb8b82da9224e234466",
-  measurementId: "G-33XTL4HJR9"
-};
-firebase.initializeApp(firebaseConfig);
+
